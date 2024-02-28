ff28ae4da3ec16d6fa7b97698ba5decdb601ec05
Walter Di Sabatino <waldisab2001@gmail.com>
2024-02-28 14:48:04 +0100

    Medico

---
+++ models/medico.py
@@ -0,0 +1,7 @@
+class Medico:
+    def __init__(self, nome, cognome, ruolo, password):
+        self.nome = nome
+        self.cognome = cognome
+        self.ruolo = ruolo
+        self.password = password
+
