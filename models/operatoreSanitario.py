006457af0229b72eef9d32f5d078af5e787bf3a1
Alessandra D'Anna <S1093790@studenti.univpm.it>
2024-02-28 15:11:16 +0100

    Update operatoreSanitario.py

---
+++ models/operatoreSanitario.py
@@ -0,0 +1,10 @@
+class OperatoreSanitario():
+    def __init__(self,nome,cognome,ruolo,password,email):
+        self.nome=nome
+        self.cognome=cognome
+        self.ruolo=ruolo
+        self.password=password
+        self.email=email
+    #deve inserire solo informazioni sullo stato di salute del paziente
+    def registerInfo():
+        return
