58ed8218a9c87a415733f0c425a9e32f6416db03
Alessandra D'Anna <S1093790@studenti.univpm.it>
2024-02-28 14:57:11 +0100

    Update paziente.py

---
+++ models/paziente.py
@@ -0,0 +1,13 @@
+class Paziente(User):
+    def __init__(self, nome, cognome, ruolo, password, email):
+        self.nome = nome
+        self.cognome = cognome
+        self.ruolo = ruolo
+        self.password = password
+        self.email = email
+
+def registerInfo():
+    pass
+
+def accessData():
+    pass
