2e01dbde12c0acbb48e91dbb1662cc483066f59c
Walter Di Sabatino <waldisab2001@gmail.com>
2024-04-13 18:26:08 +0200

    Commit

---
+++ test/pazienteContractsTest.js
@@ -0,0 +1,42 @@
+const PazienteContract = artifacts.require("PazienteContract");
+
+contract("PazienteContract", (accounts) => {
+  let pazienteContractInstance;
+
+  before(async () => {
+    pazienteContractInstance = await PazienteContract.deployed();
+  });
+
+  it("should store and retrieve cartella clinica hash correctly", async () => {
+    const paziente = accounts[0];
+    const hashDati = "hashDatiCartellaClinica";
+
+    await pazienteContractInstance.storeHashCartellaClinica(paziente, hashDati);
+    const retrievedHash = await pazienteContractInstance.retrieveHashCartellaClinica(paziente);
+
+    assert.equal(retrievedHash, hashDati, "L'hash recuperato non corrisponde all'hash memorizzato");
+  });
+
+  it("should store and retrieve visita hash correctly", async () => {
+    const medico = accounts[1];
+    const paziente = accounts[0];
+    const hashDati = "hashDatiVisita";
+
+    await pazienteContractInstance.storeHashVisita(medico, paziente, hashDati);
+    const retrievedHashes = await pazienteContractInstance.retrieveHashVisita(medico, paziente);
+
+    assert.equal(retrievedHashes.length, 1, "Il numero di hash memorizzati non è corretto");
+    assert.equal(retrievedHashes[0], hashDati, "L'hash recuperato non corrisponde all'hash memorizzato");
+  });
+
+  it("should store and retrieve farmaco hash correctly", async () => {
+    const paziente = accounts[0];
+    const hashDati = "hashDatiFarmaco";
+
+    await pazienteContractInstance.storeHashFarmaco(paziente, hashDati);
+    const retrievedHashes = await pazienteContractInstance.retrieveHashFarmaco(paziente);
+
+    assert.equal(retrievedHashes.length, 1, "Il numero di hash memorizzati non è corretto");
+    assert.equal(retrievedHashes[0], hashDati, "L'hash recuperato non corrisponde all'hash memorizzato");
+  });
+});
