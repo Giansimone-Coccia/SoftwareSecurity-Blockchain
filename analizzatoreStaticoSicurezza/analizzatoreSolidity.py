834418da9e357ca587a83555741f590c46942687
lucabellantee <s1097955@studenti.univpm.it>
2024-04-15 15:40:25 +0200

    Modificato path, aggiunto che se non esiste lo crea(il .txt) e se esiste già, lo sovrascrive

--- analizzatoreStaticoSicurezza/analizzatoreSolidity.py
+++ analizzatoreStaticoSicurezza/analizzatoreSolidity.py
@@ -1,14 +1,19 @@
 import subprocess
+import os
 
 def run_slither_on_directory():
     # Comando per eseguire Slither sulla directory corrente
     slither_command = ['slither', '.']
 
+    script_path = os.path.abspath(__file__)
+    
+    # Estrai la directory dello script
+    script_directory = os.path.dirname(script_path)
+    output_file_path = os.path.join(script_directory, "risultati_analisi_slither.txt")
     result = subprocess.run(slither_command, capture_output=True, text=True)
     
     # Scrivi l'output su un file di testo
-    with open('/Users/lauraferretti/Documents/SoftwareSecurity-Blockchain/analizzatoreStaticoSicurezza/risultati_analisi_slither.txt', 'w') as file:
-        file.write("Output di Slither:\n\n")
+    with open(output_file_path, 'w+') as file:
         file.write("Return code: {}\n\n".format(result.returncode))
         file.write("Standard Output:\n")
         file.write(result.stdout)
